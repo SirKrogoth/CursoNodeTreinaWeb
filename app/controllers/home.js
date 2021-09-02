592227b7be55b62ea0594f08562c807deedada1b
joao rafael fagundes menezes <menezes.jrafael@gmail.com>
2021-09-01 23:10:48 -0300

    Adicionado dependencias do Mongoose e implementado insercao de dados usando a Classe Curso

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,18 +1,30 @@
-var cursos = [
+/*var cursos = [
     {nome: "Unity3D", categoria: "Jogos"},
     {nome: "Express", categoria: "Back End"},
     {nome: "React", categoria: "Front End"}
-];
+];*/
 
 
 module.exports = function(app){
+    var Curso = app.models.curso;
+
     var controller = {
         index: function(req, res){
-            res.render('index', {cursos})
+            Curso.find({}, [], {sort: {nome: 1}}).exec().then((cursos)=>{
+                res.render('index', {cursos})
+            });            
         },
         newItem: function(req, res){
-            cursos.push(req.body);
-            res.json(cursos);
+            var curso = new Curso(req.body);
+
+            curso.save(function(err, curso){
+                if(err){
+                    res.status(500).end();
+                    console.err(err);
+                }else{
+                    res.json(curso);
+                }
+            })
         }
     }
 
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var app = require('./config/express')();
 const PORT = 3000;
+require('./config/database')('mongodb://localhost:27017/treinaweb');
 
 app.listen(PORT, function(){
     console.log('Servidor iniciado com sucesso' + PORT);
